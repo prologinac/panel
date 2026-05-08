6251a367d6633fef0c65c0664a7ee8872f6ff2d2
prologinac <prologinac@gmail.com>
2026-05-08 12:56:33 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,18 +4,13 @@ module.exports = {
     description: "Check bot response speed and status.",
     category: "main",
 
-    execute: async (sock, chatId, message, args, { pushname }) => {
-
-        // Calculate speed
-
+    execute: async (sock, chatId, message, args, { pushname }) => {     
         const start = Date.now();
-        // Initial response
        const { key } = await sock.sendMessage(chatId, { 
             text: `*🚀 MADRIN-MD Testing...*` 
         }, { quoted: message });
         const end = Date.now();
         const responseTime = end - start;
-        // Edit the message with the final result
         await sock.sendMessage(chatId, { 
             text: `*⚡ PONG!* \n\n> *User:* ${pushname}\n> *Speed:* ${responseTime}ms\n> *Status:* Online ✅`,
             edit: key
