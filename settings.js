72923f6b6cb36d157a2e3c051894f62414e35d66
prologinac <prologinac@gmail.com>
2026-05-08 12:52:55 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+const settings = {
+    // number
+    OWNER_NUMBER: "255780309253", 
+    // Bot Identity
+    BOT_NAME: "MADRIN",   
+CHATBOT_DM: false,
+CHATBOT_GC: false,
+    WELCOME_MESSAGE: "✅ Madrin-MD Pairing Successful!\n\nYour session is now active and linked to the bot.",
+    // Default prefix
+    PREFIX: "."
+};
+
+module.exports = settings;
+
