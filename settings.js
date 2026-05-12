dc689840fe78602caeff60df9573b220e86ddf29
prologinac <prologinac@gmail.com>
2026-05-12 15:20:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
     // number
     OWNER_NUMBER: "255780309253", 
     // Bot Identity
-    BOT_NAME: "MADRIN",   
+    BOT_NAME: "...",   
 CHATBOT_DM: false,
 CHATBOT_GC: false,
-    WELCOME_MESSAGE: "✅ Madrin-MD Pairing Successful!\n\nYour session is now active and linked to the bot.",
+    WELCOME_MESSAGE: "✅  Pairing Successful!\n\nYour session is now active and linked to the bot.",
     // Default prefix
     PREFIX: "."
 };
